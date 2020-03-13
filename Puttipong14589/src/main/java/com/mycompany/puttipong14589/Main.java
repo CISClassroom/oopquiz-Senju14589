ed8efa1a009a0d72e35be93bc5e55cc2862dd077
Senju14589 <59078952+Senju14589@users.noreply.github.com>
2020-03-13 16:15:44 +0700

    quiz

---
+++ Puttipong14589/src/main/java/com/mycompany/puttipong14589/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.puttipong14589;
+
+/**
+ *
+ * @author Lenovo Lab
+ */
+        class KeyboardInput{
+public static void main(String[] args) {    
+    String[] ListStudent = new String[] {
+        
+    }
+    }
+}
